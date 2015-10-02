6729e17f676bf7fab1bc296f934b285f9cb1fbbd
Andrew <alambrecht@lutz.us>
2015-10-02 10:43:22 -0500

    Initial checkin

---
+++ StatesHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StatesHelper")]
+[assembly: AssemblyDescription("Simple library for returning collection of US states and Canadian Provinces")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("AndyWorks")]
+[assembly: AssemblyProduct("StatesHelper")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("90a2ceb7-e9c6-4d83-a8c9-5e6df9871d94")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StatesHelper/readme.md
@@ -0,0 +1,18 @@
+﻿# States Helper
+
+Simple library for returning collection of US states and Canadian Provinces
+
+```
+public class States
+{
+    public string Name { get; set; }
+    public string Abbreviation { get; set; }
+    public string Country { get; set; }
+}
+```
+
+Usage:
+
+```
+var states = States.GetStatesFromXml();
+```
